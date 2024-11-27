0564b9bcd6f3b8c3839ffa90ee503af46a0e80d3
CocoDietrich <corentindietrich57@gmail.com>
2024-11-27 10:26:43 +0100

    Création des données du jeu

--- Kronologic/src/Main.java
+++ Kronologic/src/Main.java
@@ -1,5 +1,37 @@
+import Data.JsonReader;
+import Kronologic.Elements.Lieu;
+import Kronologic.Elements.Temps;
+import Kronologic.Enquete;
+import Kronologic.Elements.Personnage;
+import Kronologic.Indice.Indice;
+
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Enquete enquete = JsonReader.lireEnqueteDepuisJson("data/enquete_base.json");
+
+        List<Personnage> personnages = enquete.getPersonnages();
+        for (Personnage personnage : personnages) {
+            System.out.println(personnage.getNom());
+        }
+        List<Lieu> lieux = enquete.getLieux();
+        for (Lieu lieu : lieux) {
+            System.out.println(lieu.getNom());
+        }
+        List<Indice> indices = enquete.getIndices();
+        for (Indice indice : indices) {
+            System.out.println(indice.toString());
+        }
+        Personnage meurtrier = enquete.getMeurtrier();
+        System.out.println(meurtrier.getNom());
+        Lieu lieuDuCrime = enquete.getLieuDuCrime();
+        System.out.println(lieuDuCrime.getNom());
+        Temps tempsDuCrime = enquete.getTempsDuCrime();
+        System.out.println(tempsDuCrime.getTemps());
+        int loupeOr = enquete.getLoupeOr();
+        System.out.println(loupeOr);
+        int loupeBronze = enquete.getLoupeBronze();
+        System.out.println(loupeBronze);
     }
 }
