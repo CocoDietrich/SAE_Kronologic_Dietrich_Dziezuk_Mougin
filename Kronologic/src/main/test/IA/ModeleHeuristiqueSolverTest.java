88c38c64c40a4e153d71d5370aafca514febd446
Polukrados <mathieu.dziezuk@gmail.com>
2025-02-12 19:36:01 +0100

    class test heuristique

--- Kronologic/src/main/test/IA/ModeleHeuristiqueSolverTest.java
+++ Kronologic/src/main/test/IA/ModeleHeuristiqueSolverTest.java
@@ -1,4 +1,17 @@
 package IA;
 
+import Kronologic.Jeu.Elements.Lieu;
+import Kronologic.Jeu.Elements.Personnage;
+import Kronologic.Jeu.Elements.Temps;
+import org.chocosolver.solver.variables.IntVar;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ModeleHeuristiqueSolverTest {
+
+    @Test
+    public void testContrainteTemps1Personne() {
+        //TODO
+    }
 }
